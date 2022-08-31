7a6e236c9baf3c365a816178543a868f51d4f442
olamiji-8 <olamijisotomi@gmail.com>
2022-08-31 19:11:55 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Gallery from "./pages/gallery/Gallery"
 import Plans from "./pages/plans/Plans"
 import Trainers from "./pages/trainers/Trainers"
 import NotFound from "./pages/notFound/NotFound"
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
+      <NavBar/>
       <Home/>
       <About/>
       <Contact/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import "./navbar.css"
-import {Link} from "react-router-dom"
+import {Link,NavLink } from "react-router-dom"
 import Logo from "../images/logo.png"
+import {links} from "../data"
 
 const NavBar = () => {
   return (
@@ -9,7 +10,19 @@ const NavBar = () => {
     <Link to ="/" className="logo" >
     <img src={Logo} alt=""/>
     </Link>
-      Logo
+     
+     <ul>
+          {
+            links.map(({name,path}, index)=>{
+              return(
+                <li>
+                  <NavLink to={path}>{name}</NavLink>
+                </li>
+              )
+            }
+            )
+          }
+     </ul>
       nav links
       button
 
